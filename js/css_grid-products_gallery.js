341312dc2ed34aa59728c068dc81a3f0afa3df46
Hsinny <hsinnyliu@gmail.com>
2018-06-29 16:57:37 +0800

    Add 偵測圖片點擊事件

--- js/css_grid-products_gallery.js
+++ js/css_grid-products_gallery.js
@@ -15,6 +15,56 @@ $(document).ready(function () {
     });
   });
 
+  $('.item-img a').eq(0).click(function () {
+    gtag('event', 'view-more-1', {
+      'event_category': 'click',
+      'event_label': 'img'
+    });
+  });
+
+  $('.item-img a').eq(1).click(function () {
+    gtag('event', 'view-more-2', {
+      'event_category': 'click',
+      'event_label': 'img'
+    });
+  });
+
+  $('.item-img a').eq(2).click(function () {
+    gtag('event', 'view-more-3', {
+      'event_category': 'click',
+      'event_label': 'img'
+    });
+  });
+
+  $('.item-img a').eq(3).click(function () {
+    gtag('event', 'view-more-4', {
+      'event_category': 'click',
+      'event_label': 'img'
+    });
+  });
+
+  $('.item-img a').eq(4).click(function () {
+    gtag('event', 'view-more-5', {
+      'event_category': 'click',
+      'event_label': 'img'
+    });
+  });
+
+  $('.item-img a').eq(5).click(function () {
+    gtag('event', 'view-more-6', {
+      'event_category': 'click',
+      'event_label': 'img'
+    });
+  });
+
+  $('.item-img a').eq(6).click(function () {
+    console.log('7');
+    gtag('event', 'view-more-7', {
+      'event_category': 'click',
+      'event_label': 'img'
+    });
+  });
+
   $('.item-img a').hover(function () {
     gtag('event', 'view-more', {
       'event_category': 'hover',
