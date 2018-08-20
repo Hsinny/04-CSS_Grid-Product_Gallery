c12cf3ba893422ff56dec0bcf1072cf47028d537
Hsinny <hsinnyliu@gmail.com>
2018-08-20 11:03:24 +0800

    1. Hover Effect Motion 完成 2. html 組件位移改用 top、left 變動 ( 須搭 position: relative 使用 )，原先 transform: translate 留給 Hover Effect 使用。

--- js/css_grid-products_gallery.js
+++ js/css_grid-products_gallery.js
@@ -1,3 +1,89 @@
+var itemArray = [];                                      // 儲存互動元件
+var imgRect   = [];                                      // 圖片容器元件中心點
+
+
+/*===================================================================*/
+/* Hover 監聽觸發
+/*===================================================================*/
+var itemImgEl = document.querySelectorAll('.item-img');  // 圖片父層容器為觸發對象
+
+for (let i = 0; i < itemImgEl.length; i++) {
+
+  // 滑鼠移入
+  itemImgEl[i].addEventListener('mousemove', function (e) {
+    targetCenter(e);               // 算出觸發元件(圖片容器)中心點
+    motion(e, imgRect);            // 擷取滑鼠座標
+  }, false);
+
+  // 滑鼠移出，移至原位
+  itemImgEl[i].addEventListener('mouseout', function() {
+    for (let i = 0; i < itemArray.length; i++) {
+      console.log('init');
+      itemArray[i].setAttribute('style', 'translate(0,0);');
+    }
+  }, false);
+}
+
+
+/*===================================================================*/
+/* 1. 算出觸發元件(圖片容器)中心點
+/* 2. 儲存觸發元件的同層兄弟元件
+/*===================================================================*/
+function targetCenter(el){
+  var elPathItem = el.path;                             // .path 此元件的樹狀路徑，為一陣列
+  for (let i = 0; i < elPathItem.length; i++) {
+    
+    // 找出觸發元件
+    if (elPathItem[i].className === 'item-img') {       // 找出圖片容器層元件 => 觸發元件  
+      // 算出圖片元件中心點
+      var rectClient = elPathItem[i].getClientRects();  //.getClientRects() 元件佔取頁面矩形區域資訊
+      imgRect = {
+        centerX: rectClient[0].left + (rectClient[0].width / 2),
+        centerY: rectClient[0].top + (rectClient[0].height / 2)
+      }
+
+      itemArray = [];                                   // 清空之前的互動元件
+      // 觸發元件的同層兄弟元件
+      var parentEl = elPathItem[i + 1];                 // i+1 => 樹狀路徑上層 => 元件父層
+      var childEl = parentEl.childNodes;                // 找出父層下的所有子層元件 .childNodes 為陣列
+      for (let j = 0; j < childEl.length; j++) {
+        if (childEl[j].nodeName !== '#text') {          // 子層元件若為文字節點不儲存
+          itemArray.push(childEl[j]);                   // 存入互動元件陣列
+        }
+      }
+    }
+  }
+}
+
+
+/*===================================================================*/
+/* 1. 算出鼠標與中心點距離 (x1-x0, y1-y0)
+/* 2. "距離差"轉換成互動元件"位移量"
+/*===================================================================*/
+function motion(e, imgRect) {
+
+  // 擷取滑鼠座標
+  var mouseX = e.clientX,
+      mouseY = e.clientY;
+  
+  // 距離差
+  var offsetX = mouseX - imgRect.centerX,   // 例：鼠標1200 - 1080 = +120 在右側
+      offsetY = mouseY - imgRect.centerY;   // 例：鼠標530  - 600  = -70  在上測
+
+  // 互動元件位移量
+  var translateXY         = `transform: translate(${offsetX / 3}px, ${offsetY / 3}px);`,
+      translateXY_Sibling = `transform: translate(${offsetX / 6}px, ${offsetY / 6}px);`;
+
+  for (let i = 0; i < itemArray.length; i++) {          // 互動元件陣列
+    if (itemArray[i].className === 'item-img') {
+      itemArray[i].setAttribute('style', translateXY);
+    } else {
+      itemArray[i].setAttribute('style', translateXY_Sibling);
+    }
+  }
+}
+
+
 /*===================================================================*/
 /* jQuery
 /*===================================================================*/
